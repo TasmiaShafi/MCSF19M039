789b1ac1de109306aeccd98bc446a011c6d17c79
tshafi <tshafi627@gmail.com>
2021-05-30 14:12:31 +0500

    "Java and Resource files added"

---
+++ FragmentPractice/app/src/main/java/com/example/fragmentpractice/Fragment1.java
@@ -0,0 +1,23 @@
+package com.example.fragmentpractice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for thi
